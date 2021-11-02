69e220a984977cbc9cb5e9945925586741b6b9bb
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-02 23:56:52 +0900

    [update] vuex store 적용

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
+import store from "./store";
 
 Vue.config.productionTip = false;
 Vue.prototype.$axios = axios;
@@ -9,4 +10,5 @@ Vue.prototype.$axios = axios;
 new Vue({
     render: (h) => h(App),
     router,
+    store,
 }).$mount("#app");
