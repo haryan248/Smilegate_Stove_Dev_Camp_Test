368a0cbd6967521a1721653e4e8606501da0212a
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-03 21:16:47 +0900

    [add] 헤더 컴포넌트 생성

---
+++ src/components/Layout/Header/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <div class="Header-header-container">
+            <router-link to="/">
+                <h2 class="Header-header-title">STOVE DEV CAMP</h2>
+            </router-link>
+
+            <router-link to="/blog/write">
+                <div class="Header-header-write__button">글 쓰기</div>
+            </router-link>
+        </div>
+    </header>
+</template>
+<script>
+export default {
+    name: "Header",
+};
+</script>
+<style scoped>
+@import "./Header.css";
+</style>
