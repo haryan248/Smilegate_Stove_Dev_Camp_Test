b45d65c6afade0cc56099742adfd3ac41ca0cc7e
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-03 21:16:15 +0900

    [update] 상세 페이지 css 적용

--- src/pages/DetailPage/DetailPage.vue
+++ src/pages/DetailPage/DetailPage.vue
@@ -1,14 +1,18 @@
 <template>
-    <div>
-        <div>제목 : {{ blogContent.title }}</div>
-        <div>본문 : {{ blogContent.description }}</div>
-        <div>등록일 : {{ blogContent.created_at }}</div>
-        <button @click="updateBlogPost(blogContentId)">글 수정하기</button>
-        <button @click="deleteBlogPost(blogContentId)">글 삭제하기</button>
+    <div class="DetailPage-container">
+        <h2 class="DetailPage-title">{{ blogContent.title }}</h2>
 
+        <div class="DetailPage-content--container">
+            <div class="DetailPage-management">
+                <div class="DetailPage-create__date">{{ blogContent.created_at }}</div>
+                <button class="DetailPage-modify__button" @click="updateBlogPost(blogContentId)">수정하기</button>
+                <button class="DetailPage-delete__button" @click="deleteBlogPost(blogContentId)">삭제하기</button>
+            </div>
+            <div class="DetailPage-description" v-html="blogContent.description"></div>
+        </div>
         <div>
-            <div>{{ blogContent.commentCount }} 개의 댓글</div>
-            <div>댓글 목록</div>
+            <div class="DetailPage-comment__count">{{ blogContent.commentCount }} 개의 댓글</div>
+            <div class="DetailPage-comment__list">댓글 목록</div>
             <div>
                 <CommentList :blogContentId="blogContentId" />
             </div>
