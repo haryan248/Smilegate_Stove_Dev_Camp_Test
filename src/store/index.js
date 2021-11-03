b70b372bba5061b1fca352b0c2069c6337408317
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-03 14:54:37 +0900

    [update] 댓글 스토어-> Root 스토어에 등록

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import postStore from "./modules/postStore.js";
+import commentStore from "./modules/commentStore.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
-        postStore: postStore,
+        postStore,
+        commentStore,
     },
 });
