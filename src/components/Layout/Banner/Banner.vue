7204d10c05f8dedb9918a249686dd3af29b19f40
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-05 10:04:25 +0900

    [update] 배너 컴포넌트 생성

---
+++ src/components/Layout/Banner/Banner.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="Banner-container">
+        <div class="Banner-banner-wrapper">
+            <div class="Banner-banner">
+                <div class="Banner-banner-design type1"></div>
+                <div class="Banner-banner-design type2"></div>
+                <div class="Banner-banner-design type3"></div>
+            </div>
+            <div class="Banner-banner-text">블로그에 오신걸 환영합니다.</div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "Banner",
+};
+</script>
+<style scoped>
+@import "./Banner.css";
+</style>
--- src/pages/HomePage/HomePage.vue
+++ src/pages/HomePage/HomePage.vue
@@ -1,16 +1,6 @@
 <template>
     <div class="HomePage-container">
-        <div class="HomePage-my-profile">
-            <div class="HomePage-profile-detail">
-                <div class="HomePage-banner">
-                    <div class="HomePage-banner-design type1"></div>
-                    <div class="HomePage-banner-design type2"></div>
-                    <div class="HomePage-banner-design type3"></div>
-                </div>
-                <div class="HomePage-banner-text">블로그에 오신걸 환영합니다.</div>
-            </div>
-        </div>
-
+        <Banner />
         <div class="HomePage-post__list--container">
             <h3 class="HomePage-post--title">게시글 목록</h3>
             <div class="HomePage-post-empty--container" v-if="blogPosts.length === 0">
@@ -39,6 +29,7 @@
     </div>
 </template>
 <script>
+import Banner from "../../components/Layout/Banner/Banner.vue";
 import { createNamespacedHelpers } from "vuex";
 const postHelper = createNamespacedHelpers("postStore");
 
@@ -46,6 +37,9 @@ export default {
     name: "HomePage",
     data() {
         return {};
+    },
+    components: {
+        Banner,
     },
     mounted() {
         this.getBlogPostData();
